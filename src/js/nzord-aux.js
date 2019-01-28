7442ce9327ba825d01261c78af339e5138d0ae1d
Eduardo Avila Malheiros <edu.avila2@gmail.com>
2019-01-28 07:13:23 -0400

    # Add funcao para gerar tablesExcel.

--- src/js/nzord-aux.js
+++ src/js/nzord-aux.js
@@ -61,6 +61,42 @@ function generateexcel(tableid) {
     while (html.indexOf('ç') != -1) html = html.replace('ç', '&ccedil;'); 
         
 
-    window.open('data:application/vnd.ms-excel,' + encodeURIComponent(html));
+    window.open('data:application/vnd.ms-excel,' + encodeURIComponent(html) + '.xlsx');
+}
+
+
+function generateExcelHtml(tableid) {
+    var table= document.getElementById(tableid);
+    var html = table.outerHTML;
+    while (html.indexOf('á') != -1) html = html.replace('á', '&aacute;');
+    while (html.indexOf('ã') != -1) html = html.replace('ã', '&atilde;');
+    while (html.indexOf('â') != -1) html = html.replace('â', '&acirc;');
+    while (html.indexOf('Á') != -1) html = html.replace('Á', '&Aacute;');
+    while (html.indexOf('À') != -1) html = html.replace('À', '&Agrave;');
+    while (html.indexOf('Ã') != -1) html = html.replace('Ã', '&Atilde;');
+    while (html.indexOf('Â') != -1) html = html.replace('Â', '&Acirc;');
+    while (html.indexOf('é') != -1) html = html.replace('é', '&eacute;');
+    while (html.indexOf('ê') != -1) html = html.replace('ê', '&ecirc;');
+    while (html.indexOf('É') != -1) html = html.replace('É', '&Eacute;');
+    while (html.indexOf('Ê') != -1) html = html.replace('Ê', '&Ecirc;');
+    while (html.indexOf('í') != -1) html = html.replace('í', '&iacute;');
+    while (html.indexOf('Í') != -1) html = html.replace('Í', '&Iacute;');
+    while (html.indexOf('ó') != -1) html = html.replace('ó', '&oacute;');
+    while (html.indexOf('õ') != -1) html = html.replace('õ', '&otilde;');
+    while (html.indexOf('ô') != -1) html = html.replace('ô', '&ocirc;');
+    while (html.indexOf('Ó') != -1) html = html.replace('Ó', '&Oacute;');
+    while (html.indexOf('Ô') != -1) html = html.replace('Ô', '&Ocirc;');
+    while (html.indexOf('Õ') != -1) html = html.replace('Õ', '&Otilde;');
+    while (html.indexOf('ú') != -1) html = html.replace('ú', '&uacute;');
+    while (html.indexOf('Ú') != -1) html = html.replace('Ú', '&Uacute;');
+    while (html.indexOf('º') != -1) html = html.replace('º', '&ordm;');
+    while (html.indexOf('²') != -1) html = html.replace('²', '&sup2;');
+    while (html.indexOf('ñ') != -1) html = html.replace('ñ', '&ntilde;'); 
+    while (html.indexOf('Ñ') != -1) html = html.replace('Ñ', '&Ntilde;');  
+    while (html.indexOf('Ç') != -1) html = html.replace('Ç', '&Ccedil;');  
+    while (html.indexOf('ç') != -1) html = html.replace('ç', '&ccedil;'); 
+        
+
+    return 'data:application/vnd.ms-excel,' + encodeURIComponent(html);
 }
 
